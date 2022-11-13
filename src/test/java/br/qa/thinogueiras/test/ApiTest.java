6949961038283b4c37ed415426af6ec2d3c97636
thinogueiras <thiagonogueira@hotmail.com.br>
2022-11-13 00:01:33 -0300

    Added two new tests

--- src/test/java/br/qa/thinogueiras/test/ApiTest.java
+++ src/test/java/br/qa/thinogueiras/test/ApiTest.java
@@ -1,12 +1,34 @@
 package br.qa.thinogueiras.test;
 
-import static io.restassured.RestAssured.*;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import br.qa.thinogueiras.core.BaseTest;
 
 public class ApiTest extends BaseTest{
+	
+	private static String TOKEN;
+	
+	@BeforeAll
+	public static void setToken() {
+		Map<String, String> login = new HashMap<>();
+		login.put("email", "thinogueiras@testing.com");
+		login.put("senha", "test123");
+		
+		TOKEN = given()			
+			.body(login)
+		.when()
+			.post("/signin")
+		.then()
+			.statusCode(200)
+			.extract().path("token");
+	}
 	
 	@Test
 	public void shouldNotBeAccessWithoutToken() {
@@ -15,5 +37,28 @@ public class ApiTest extends BaseTest{
 				.get("/contas")
 			.then()
 				.statusCode(401);
+	}
+	
+	@Test
+	public void shouldBeInsertAccount() {		
+		given()
+			.header("Authorization", "JWT " + TOKEN)
+			.body("{\"nome\": \"Conta básica\"}")
+		.when()
+			.post("/contas")
+		.then()
+			.statusCode(201);
+	}
+	
+	@Test
+	public void shouldBeChangeAccount() {
+		given()			
+			.header("Authorization", "JWT " + TOKEN)
+			.body("{\"nome\": \"Conta alterada\"}")
+		.when()
+			.put("/contas/1477462")
+		.then()
+			.statusCode(200)
+			.body("nome", is("Conta alterada"));
 	}
 }
