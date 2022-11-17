7a56e5e33572f9c4183190ccc87be71fd8721572
thinogueiras <thiagonogueira@hotmail.com.br>
2022-11-17 01:19:03 -0300

    Util Class Methods

---
+++ src/test/java/br/qa/thinogueiras/utils/Utils.java
@@ -0,0 +1,31 @@
+package br.qa.thinogueiras.utils;
+
+import static io.restassured.RestAssured.get;
+
+import br.qa.thinogueiras.core.Movement;
+
+public class Utils {
+	
+	public static Movement getValidMovement() {
+		Movement movement = new Movement();
+				
+		movement.setConta_id(getAccountIdByName("Conta para movimentacoes"));
+		movement.setDescricao("Teste de inserção de movimentação");
+		movement.setEnvolvido("Env mov");
+		movement.setTipo("REC");
+		movement.setData_transacao("13/11/2022");
+		movement.setData_pagamento("15/11/2026");
+		movement.setValor(5000f);
+		movement.setStatus(true);
+		
+		return movement;
+	}
+
+	public static Integer getAccountIdByName(String name) {
+		return get("/contas?nome="+name).then().extract().path("id[0]");
+	}
+	
+	public static Integer getMovementIdByDescription(String description) {
+		return get("/transacoes?descricao="+description).then().extract().path("id[0]");
+	}
+}
