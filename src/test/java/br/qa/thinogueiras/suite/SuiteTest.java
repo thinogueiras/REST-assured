41f8d5670ef54584ca626c3f538937eb9c041da0
Thiago Nogueira dos Santos <thiagonogueira@hotmail.com.br>
2023-10-11 17:05:41 -0300

    Refatoração na classe da suíte de testes

--- src/test/java/br/qa/thinogueiras/suite/SuiteTest.java
+++ src/test/java/br/qa/thinogueiras/suite/SuiteTest.java
@@ -1,21 +1,12 @@
 package br.qa.thinogueiras.suite;
 
-import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 import org.junit.platform.suite.api.SuiteDisplayName;
 
-import br.qa.thinogueiras.tests.AccountTest;
-import br.qa.thinogueiras.tests.AuthTest;
-import br.qa.thinogueiras.tests.BalanceTest;
-import br.qa.thinogueiras.tests.MovementTest;
-
 @Suite
 @SuiteDisplayName("API Tests - Barriga REST")
-@SelectClasses({ 
-	AuthTest.class,
-	AccountTest.class, 
-	BalanceTest.class,
-	MovementTest.class }) 
+@SelectPackages("br.qa.thinogueiras.tests")
 public class SuiteTest {	
-	
+
 }
